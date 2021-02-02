d8d996774f25346ab632933f2197ef96b8773cd3
yujisu01 <jssla1@naver.com>
2021-02-02 14:32:43 +0900

    파일

---
+++ Java_002/src/Hello.java
@@ -0,0 +1,13 @@
+package com.callor.hello
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.println("30 + 40 = 70")
+	System.out.println("30 * 40 = 1200")
+	System.out.println("40 - 30  = 10")
+	System.out.println("40 / 20 = 2")
+	System.out.println("여러분 반갑습니다")
+	}
+
+}
