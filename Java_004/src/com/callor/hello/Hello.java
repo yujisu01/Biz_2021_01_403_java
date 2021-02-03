08b18408e59dbb8e968b6e02f4ee910bcbe62854
yujisu01 <jssla1@naver.com>
2021-02-03 09:30:56 +0900

    README파일 수정

--- Java_004/src/com/callor/hello/Hello.java
+++ Java_004/src/com/callor/hello/Hello.java
@@ -2,4 +2,22 @@ package com.callor.hello;
 
 public class Hello {
 
+	public static void main(String[] args) {
+		
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		
+		int num1 = 68;
+		int num2 = 55;
+		
+		
+	System.out.println(num1 + num2);
+	System.out.println(num1 * num2);
+	System.out.println(num2 - num1);
+	System.out.println(num2 / num1);
+	System.out.println(num2 % num2);
+	
+	}
+	
+	
 }
