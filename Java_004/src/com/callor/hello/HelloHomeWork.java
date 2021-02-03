46989be7097f69a03928827e74f146e0d9a1369f
yujisu01 <jssla1@naver.com>
2021-02-03 10:30:44 +0900

    집에서 연습

---
+++ Java_004/src/com/callor/hello/HelloHomeWork.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+/* 이 클래스는 집에서 공부를 위해 추가한것
+ * 
+ */
+
+public class HelloHomeWork {
+	
+	public static void main(String[] args) {
+		System.out.println("반갑습니다");
+	}
+
+}
