8ed19674c925ba6f15e9aac728b340b487283e13
yujisu01 <jssla1@naver.com>
2021-02-04 16:33:38 +0900

    반복문

---
+++ Java_006/src/com/callor/varriable/Loop_05.java
@@ -0,0 +1,28 @@
+package com.callor.varriable;
+
+public class Loop_05 {
+	
+	public static void main(String[] args) {
+		
+		int intDan = 7;
+		for(int num = 1; num < 10 ; num += 1) {
+			
+		}
+		for(int num = 1; num < 100 ; num += 1) {   // 표준적인 for반복문 코드이다
+			
+		}
+		for(int num = 20 ; num < 42 ; num += 1) { 
+			System.out.println("안녕");
+	}	
+		//	시작하는값	  비교하는값   증가하는값
+		for(int num = 12 ; num <= 20 ; num += 1) {
+			System.out.println("반가워");
+		}
+		for(int num = 12 ; num <= 20 ; num += 2) {
+			System.out.println(num);
+		}
+
+
+	}
+
+}
