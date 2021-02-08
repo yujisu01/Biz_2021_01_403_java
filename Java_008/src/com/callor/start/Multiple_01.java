26983d39e94b41b4e5b62c528f600c9a0e951cc5
yujisu01 <jssla1@naver.com>
2021-02-08 13:34:32 +0900

    EvenOdd

--- Java_008/src/com/callor/start/Multiple_01.java
+++ Java_008/src/com/callor/start/Multiple_01.java
@@ -6,6 +6,7 @@ public class Multiple_01 {
 		int sum = 0;
 		int num = 0;
 
+		// 7~106
 		for (int i = 0; i < 100; i++) {
 
 			num = i + 7;
@@ -13,10 +14,10 @@ public class Multiple_01 {
 			boolean bYes = num % 3 == 0;
 			if (bYes) {
 				System.out.println("3의 배수: " + num);
-			sum += num;
+				sum += num;
 			}
 		}
-				System.out.println("3의 배수들의 합 : " + sum);
+		System.out.println("3의 배수들의 합 : " + sum);
 	}
 
 }
--- Java_008/src/com/callor/start/Odd_Even_02.java
+++ Java_008/src/com/callor/start/Odd_Even_02.java
@@ -1,25 +1,51 @@
-package com.callor.start;
+	package com.callor.start;
 
-public class Odd_Even_02 {
-	public static void main(String[] args) {
+	public class Odd_Even_02 {
 		
+		public static void main(String[] args) {
 		
+		int intSumEven = 0;
 		
-		// 위에서 선언하고 사용했던 변수를 "재사용"할때는
-		// 반드시 초기화(Clear)해야 한다.
+		for(int i = 0 ; i < 100 ; i++) {
+			
+			int num = i + 1;
+			
+			boolean bYes = num % 2 == 0;
+			if(bYes) {
+				intSumEven += num;
+			}
+		}
+		System.out.println("짝수의 합 : " + intSumEven);
+		
 		int intSumOdd = 0;
 		for(int i = 0 ; i < 100; i++) {
 			int num = i + 1;
+			boolean bYes = num % 2 == 1;
+			if(bYes) {
+				intSumOdd += num;
+			}
+		}
+		System.out.println("홀수의 합 : " + intSumOdd);
+		
+		// 위에서 선언하고 사용했던 변수를 "재사용" 하려면
+		// 반드시 초기화(clear) 해야 한다.
+		// 이게 더 좋은 코드 
+		
+		intSumOdd = 0;
+		for(int i = 0 ; i < 100 ; i++) {
+			int num = i + 1;
 			boolean bYes = num % 2 == 0;
 			
-			// if(bYes == false) {
+			// if( bYes == false) {
 			
 			// bYes가 true가 아니면
 			if( !bYes ) {
-				intSumOdd += num;
+				intSumOdd += num;	
+			}
 		}
-	}
 		System.out.println("홀수의 합 : " + intSumOdd);
-
-}
+		
+		
+		
+	}
 }
