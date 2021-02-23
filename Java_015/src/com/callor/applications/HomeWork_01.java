1ded6a592ade546abbc198ac4986588fb2bd2bc3
yujisu01 <jssla1@naver.com>
2021-02-24 08:41:01 +0900

    Prime

--- Java_015/src/com/callor/applications/HomeWork_01.java
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -1,33 +1,40 @@
 package com.callor.applications;
 
 import java.util.Random;
-
-import com.callor.applications.service.PrimeServiceV2T;
+import com.callor.applications.service.PrimeServiceV2;
 
 public class HomeWork_01 {
 
 	public static void main(String[] args) {
 
-		PrimeServiceV2T psV2 = new PrimeServiceV2T();
+		PrimeServiceV2 psV2 = new PrimeServiceV2();
 		Random rnd = new Random();
-		
+
 		int[] intNum = new int[10];
-		
-		
-		for (int i = 0; i < intNum.length; i++) {
-		intNum[i] = rnd.nextInt(100);
-		System.out.println("정수 출력:" + intNum[i]);
+
+		System.out.println("정수 출력");
+		System.out.println("======================");
+
+		for (int i = 2; i < intNum.length; i++) {
+			intNum[i] = rnd.nextInt(100);
+			System.out.println(intNum[i]);
 		}
-		
 		int intSum = 0;
-		for (int i = 0; i < intNum.length; i++) {
+
+		System.out.println("소수출력");
+		System.out.println("=======================");
+
+		for (int i = 2; i < intNum.length; i++) {
 			int result = psV2.prime(intNum[i]);
+		
 			if (result > 0) {
-				System.out.println("소수들: " + intNum[i]);
+				System.out.println(intNum[i]);
+
 				intSum += intNum[i];
+
 			}
-			
-			
-		}System.out.println("소수인 수들의 합계:" + intSum);
+		}
+		System.out.println("소수인 수들의 합계:" + intSum);
+
 	}
 }
